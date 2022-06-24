7416280017d6acebe140ea5abac49493c1b28e3c
Sonia RENARD <s_renard1@etu-webschoolfactory.fr>
2022-06-24 10:12:41 +0200

    css navbar correction et correction du placeholder pour mieux afficher le texte

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ import montre1 from './image/montre1.jpg';
 const Home =() => {
   return (
     <div className='divhome'>
-      <h1 className='titleprincipal'>Bienvenue sur notre site de référencement des événements horlogers </h1>
+      <h1 className='titleprincipal'>Bienvenue sur notre site de référencement <br/> des événements horlogers </h1>
       <img className='imgprincipal' src={montre1} alt="imageprincipale"></img>
-      <h3> Explorez notre Sélection d'évenements autour de montres prestigieuses de haute précision. Nous proposons un vaste choix d'évenements pour convenir à tous les goûts et toutes les envies. Découvrer l'univers de l'horlogerie à votre façon </h3>
+      <h3> Explorez notre Sélection d'évenements autour de montres prestigieuses de haute précision. <br/> Nous proposons un vaste choix d'évenements pour convenir à tous les goûts et toutes les envies. <br/> Découvrer l'univers de l'horlogerie à votre façon </h3>
     </div>
   );
 };
